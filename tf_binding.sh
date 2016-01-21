8b4ce9dce92f01c07c3e2f89c4609c7fa54c77f6
Matt Abraham <kreighoff93@gmail.com>
2016-01-20 19:20:54 -0800

    exercises 1-3 are now added

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,3 +1,12 @@
+#exercise 1
+grep 'NFKB' tf.bed > tf.nfkb.bed
+
+#exercise 2
+awk '$3 == "transcript"' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
+
+#exercise 3
+bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
+
 #exercise 4
 module load biotools
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
