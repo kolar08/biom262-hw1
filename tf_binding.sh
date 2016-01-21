39ec6e21241ab1ef8c63a1b9f965671569ed92b6
akpribadi <apribadi@ucsd.edu>
2016-01-20 17:02:48 -0800

    Create tf_binding.sh

---
+++ tf_binding.sh
@@ -0,0 +1,6 @@
+#exercise 4
+module load biotools
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+
+#exercise 5
+bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.nfkb.fasta
