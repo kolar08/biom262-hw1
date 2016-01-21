441722d89afff628e572c5417cf5c21b5989a0a6
Amy Pribadi <apribadi@ucsd.edu>
2016-01-20 21:09:59 -0800

    added the 2

--- tf_binding.sh
+++ tf_binding.sh
@@ -14,4 +14,4 @@ bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf
 #exercise 5
 bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.nfkb.fasta
 
-echo "Hello I am a message in standard error (stderr)" >&
+echo "Hello I am a message in standard error (stderr)" >&2
