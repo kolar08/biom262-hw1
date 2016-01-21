0466fa60abfd8e9480318efbf9c9819f5ec1202d
Amy Pribadi <apribadi@ucsd.edu>
2016-01-20 21:11:52 -0800

    changed ex 5 bed file

--- tf_binding.sh
+++ tf_binding.sh
@@ -12,6 +12,6 @@ module load biotools
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 
 #exercise 5
-bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.nfkb.fasta
+bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed tf.nfkb.bed -fo gencode.v19.annotation.chr22.transcript.nfkb.fasta
 
 echo "Hello I am a message in standard error (stderr)" >&2
